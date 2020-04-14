89e06e6e3f0b51aca9653854497630cc487e8501
Rafi Haidari <heblex.afg@gmail.com>
2020-04-14 10:38:01 +0430

    reverted

--- index.php
+++ index.php
@@ -5,3 +5,9 @@ echo "Hello Git!";
 echo "New Changes!";
 
 echo "a new line of code added inot develop.";
+ function login($user, $pwd)
+{
+    if($user == 'admin' && $pwd === 123){
+        return true;
+    }
+}
